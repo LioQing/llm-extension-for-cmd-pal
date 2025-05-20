f4db5ec92f4fe1dd2a9d17bbbd0ec1c51184dcdd
LioQing <s2010157@gmail.com>
2025-05-20 11:20:00 +0800

    Fix bug related to message history

--- LlmExtension/Pages/LlmExtensionPage.cs
+++ LlmExtension/Pages/LlmExtensionPage.cs
@@ -91,13 +91,13 @@ internal sealed partial class LlmExtensionPage : DynamicListPage
 
             ChatHistory history = [];
             history.AddSystemMessage(Config.System);
-            foreach (var message in messages)
+            foreach (var message in messages.Reverse())
             {
                 if (!string.IsNullOrEmpty(message.User))
                 {
                     history.AddUserMessage(message.User);
 
-                    if (history.Count > Config.History + 1)
+                    if (history.Count >= Config.History + 1)
                     {
                         break;
                     }
@@ -107,7 +107,7 @@ internal sealed partial class LlmExtensionPage : DynamicListPage
                 {
                     history.AddAssistantMessage(message.Assistant);
 
-                    if (history.Count > Config.History + 1)
+                    if (history.Count >= Config.History + 1)
                     {
                         break;
                     }
@@ -386,7 +386,7 @@ internal sealed partial class LlmExtensionPage : DynamicListPage
                 }
 
                 return _commands
-                    .OrderBy(c => Levenshtein(c.Key, commandText))
+                    .OrderByDescending(c => c.Key.Zip(commandText).TakeWhile((pair) => pair.First == pair.Second).Count())
                     .Select(c =>
                     {
                         ICommand command = c.Value.Item5 != null
@@ -567,59 +567,6 @@ internal sealed partial class LlmExtensionPage : DynamicListPage
         RaiseItemsChanged(_messages.Count);
     }
 
-    private static int Levenshtein(string a, string b)
-    {
-
-        if (string.IsNullOrEmpty(a))
-        {
-            if (!string.IsNullOrEmpty(b))
-            {
-                return b.Length;
-            }
-            return 0;
-        }
-
-        if (string.IsNullOrEmpty(b))
-        {
-            if (!string.IsNullOrEmpty(a))
-            {
-                return a.Length;
-            }
-            return 0;
-        }
-
-        int cost;
-        int[,] d = new int[a.Length + 1, b.Length + 1];
-        int min1;
-        int min2;
-        int min3;
-
-        for (int i = 0; i <= d.GetUpperBound(0); i += 1)
-        {
-            d[i, 0] = i;
-        }
-
-        for (int i = 0; i <= d.GetUpperBound(1); i += 1)
-        {
-            d[0, i] = i;
-        }
-
-        for (int i = 1; i <= d.GetUpperBound(0); i += 1)
-        {
-            for (int j = 1; j <= d.GetUpperBound(1); j += 1)
-            {
-                cost = (a[i - 1] != b[j - 1]) ? 1 : 0;
-
-                min1 = d[i - 1, j] + 1;
-                min2 = d[i, j - 1] + 1;
-                min3 = d[i - 1, j - 1] + cost;
-                d[i, j] = Math.Min(Math.Min(min1, min2), min3);
-            }
-        }
-
-        return d[d.GetUpperBound(0), d.GetUpperBound(1)];
-
-    }
     internal static void ErrorToast(string message)
     {
         new ToastStatusMessage(new StatusMessage()
